c31da115c8cf4e50f959204213527475b746f2f6
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-07-25 11:11:09 +0530

    completed the models for the app

--- server/models/passwordModel.js
+++ server/models/passwordModel.js
@@ -3,6 +3,22 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.schema({
     userId:{
-        type:mongoose.schema.Types
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    password:{
+        type:Object,
+        required:true
+    },
+    appName:{
+        type:String,
+        required:true
+    },
+    userName:{
+        type:String,
+        required:true
     }
 })
+
+const passwordModel=mongoose.model('password',schema)
+export default passwordModel
